fad85f87d4b12dbc11045d6ea90076bf420a7bae
Tejas <tejas1379antal@gmail.com>
2025-07-15 13:16:13 +0530

    Mobile-NavBar-Build

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -4,6 +4,10 @@ import Walmart from '../../assets/walmart.png'
 import Adobe from '../../assets/adobe.png'
 import Microsoft from '../../assets/microsoft.png'
 import Facebook from '../../assets/facebook.png'
+import FacebookIcon from '../../assets/facebook-icon.png'
+import TwitterIcon from '../../assets/twitter.png'
+import YoutubeIcon from '../../assets/youtube.png'
+import InstagramIcon from '../../assets/instagram.png'
 
 const Contact = () => {
   return (
@@ -19,7 +23,20 @@ const Contact = () => {
             </div>
         </div>
         <div className="contact">
-
+            <h1 className="contactPageTitle">Contact Me</h1>
+            <span className="contactDesc">Please fill out the form below to discuss any work opportunity.</span>
+            <form action="" className="contactForm">
+              <input type="text" className="name"  placeholder='Your Name'/>
+              <input type="email" className="email" placeholder='Your Email'/>
+              <textarea className='msg' name="message" id="5" placeholder='Your Message'></textarea>
+              <button type='submit' value='Send' className="submitBtn">Submit</button>
+              <div className="links">
+                <img src={FacebookIcon} alt="Facebook" className="link" />
+                <img src={TwitterIcon} alt="Twitter" className="link" />
+                <img src={YoutubeIcon} alt="Youtube" className="link" />
+                <img src={InstagramIcon} alt="Instagram" className="link" />
+              </div>
+            </form>
         </div>
     </section>
   )
